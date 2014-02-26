79bf5954385ab764ecf97856876a21cd8a2a196a
Ondřej Čertík <ondrej.certik@gmail.com>
2014-02-26 16:52:54 -0700

    Export get_hydrogen_energies()

--- src/dftatom.f90
+++ src/dftatom.f90
@@ -21,6 +21,7 @@ public dp, E_nl, get_atomic_states_nonrel, mesh_exp, stop_error, &
         solve_radial_eigenproblem, get_tf_energies, thomas_fermi_potential, &
         get_atomic_states_rel, integrate, get_Vxc, str, mesh_exp_deriv, &
         get_atom_orb, atom_lda, atom_rlda, rpoisson_outward_pc, &
-        get_atom_orb_rel, integrate_rproblem_outward, get_Vh
+        get_atom_orb_rel, integrate_rproblem_outward, get_Vh, &
+        get_hydrogen_energies
 
 end module
