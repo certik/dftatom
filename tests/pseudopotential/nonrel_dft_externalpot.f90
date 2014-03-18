8c1b91c5a21b988a040a715a2cf4c934ee7e48d6
Ondřej Čertík <ondrej.certik@gmail.com>
2014-03-17 23:54:51 -0600

    Polish the example

--- tests/pseudopotential/nonrel_dft_externalpot.f90
+++ tests/pseudopotential/nonrel_dft_externalpot.f90
@@ -4,14 +4,12 @@ use dftatom, only: dp, mesh_exp, mesh_exp_deriv, thomas_fermi_potential, E_nl, &
 use interpolation, only: spline3, loadtxt
 use utils, only: assert, newunit
 use ode1d, only: integrate
-use constants, only: pi
 implicit none
 
 ! Mesh parameters:
 real(dp), parameter :: r_min = 1e-7_dp, r_max = 50.0_dp, a = 2.7e6_dp
 integer, parameter :: N = 5000
 real(dp), target :: R(N+1), Rp(N+1)
-integer :: Z
 !integer, parameter :: n_orb = 11
 integer, parameter :: n_orb = 3
 integer, target :: no(n_orb), lo(n_orb)
@@ -34,32 +32,14 @@ real(dp), allocatable :: data(:, :)
 real(dp) :: Ekin, Eee, Een, Exc, Etot, Enl
 integer :: xc_type
 
-Z = 14
-! Configuration for Z=50:
-!no = [1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5]
-!lo = [0, 0, 1, 0, 1, 2, 0, 1, 2, 0, 1]
-!fo = [2, 2, 6, 2, 6, 10, 2, 6, 10, 2, 2]
-! You can modify the occupation here:
-!fo(10) = 1.5_dp
-!fo(11) = 2.0_dp
-
 no = [1, 2, 3]
 lo = [0, 1, 2]
 fo = [2, 2, 10]
 
-!no = [1]
-!lo = [0]
-!fo = [2]
-
 R = mesh_exp(r_min, r_max, a, N)
 Rp = mesh_exp_deriv(r_min, r_max, a, N)
 
-!ks_energies = [-2, -1] ! Initial guess for energies
-!ks_energies = get_hydrogen_energies(Z, no)
-ks_energies(1) = -0.738977_dp
-ks_energies(2) = -0.738977_dp
-ks_energies(3) = -0.738977_dp
-V_tot = thomas_fermi_potential(R, Z) ! Initial guess for the potential
+ks_energies = [-2, -1, -1] ! Initial guess for energies
 
 ! R = data(1, :)
 ! V0 = data(2, :)
@@ -77,16 +57,7 @@ V_tot = V_loc ! Initial guess for the total potential
 
 ! We allow a few unbounded states
 Emax_init = 10
-! Use Hydrogen Schroedinger energies for the lower limit:
-do i = 1, size(no)
-    Emin_init(i) = E_nl(0._dp, no(i), lo(i), Z, 0)
-end do
-Emin_init(1) = -0.738977_dp
-Emin_init(1) = -30
-Emin_init(2) = -30
-Emin_init(3) = -30
-! For robustness, decrease Emin by 10%:
-Emin_init = 1.5_dp * Emin_init
+Emin_init = -30
 
 xc_type = 2
 call atom_lda_pseudo(no, lo, fo, Emin_init, Emax_init, ks_energies, &
