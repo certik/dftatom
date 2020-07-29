18d4ceb95f2299e17a809860ca9e4bea328493ec
Ondřej Čertík <ondrej@certik.us>
2020-07-29 11:31:28 -0600

    Use the HGH pseudopotential

--- tests/enrichment/enrichment.f90
+++ tests/enrichment/enrichment.f90
@@ -3,22 +3,33 @@ use dftatom, only: mesh_exp, mesh_exp_deriv, dp, &
     solve_radial_eigenproblem, stop_error
 implicit none
 
-! Atomic number:
-integer, parameter :: Z = 92
 ! Mesh parameters:
 real(dp), parameter :: r_min = 1e-8_dp, r_max = 50.0_dp, a = 1e6_dp
-integer, parameter :: NN = 3000
+integer, parameter :: NN = 50000
 
 real(dp), parameter :: c = 137.035999037_dp, eps = 1e-9_dp
 integer :: n, l, relat, converged
 real(dp) :: R(NN+1), u(size(r)), Ein, E, E_exact, error
 real(dp) :: P(size(R)), Q(size(R)), Rp(NN+1)
+real(dp) :: Zion, rloc, C1, C2 !, C3, C4
 logical, parameter :: perturb = .true.
 
 
 R = mesh_exp(r_min, r_max, a, NN)
 Rp = mesh_exp_deriv(r_min, r_max, a, NN)
-u(:) = -Z/r
+!u(:) = -Z/r
+
+rloc = 0.2_dp
+C1 = -4.180237_dp
+C2 =  0.725075_dp
+!C3 = 0
+!C4 = 0
+Zion = 1
+
+u(:) = - Zion/r * erf(r/(sqrt(2._dp)*rloc)) &
+             + exp(-1._dp/2*(r/rloc)**2) * (C1 + C2*(r/rloc)**2)
+!u = -Zion/r
+
 
 print *, "Hydrogen like energies for Z=92 (U)"
 print *, "Mesh parameters (r_min, r_max, a, N):"
@@ -28,10 +39,11 @@ print "(A3, A3, A15, A15, A10)", "n", "l", "E", "E_exact", "Error"
 print *
 do n = 1, 7
     do l = 0, n-1
-        E_exact = - Z**2 / (2.0_dp * n**2)
+        E_exact = - Zion**2 / (2.0_dp * n**2)
         Ein = -1000
         relat = 0
-        call solve_radial_eigenproblem(n, l, Ein, eps, 100, R, Rp, u, Z, c, &
+        call solve_radial_eigenproblem(n, l, Ein, eps, 100, R, Rp, u, &
+            int(Zion), c, &
             relat, perturb, -5000._dp, 0._dp, converged, E, P, Q)
         error = abs(E -E_exact)
         if (converged /= 0) call stop_error("Not converged")
