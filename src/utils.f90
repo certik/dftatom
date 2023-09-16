33dec0128e6e42b2d9e4687fec63fcff5c8c85b3
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-09-16 23:33:00 +0530

    Remove str_real_len, str_real and str_real_n for now

--- src/utils.f90
+++ src/utils.f90
@@ -9,7 +9,7 @@ public upcase, lowcase, white_char, blank, num_strings, get_string, &
     stop_error, savetxt, newunit, assert, str
 
 interface str
-    module procedure str_int, str_real, str_real_n
+    module procedure str_int
 end interface
 
 contains
@@ -227,34 +227,6 @@ pure function str_int(i) result(s)
 integer, intent(in) :: i
 character(len=str_int_len(i)) :: s
 write(s, '(i0)') i
-end function
-
-pure integer function str_real_len(r, fmt) result(sz)
-! Returns the length of the string representation of 'i'
-real(dp), intent(in) :: r
-character(len=*), intent(in) :: fmt
-integer, parameter :: MAX_STR = 100
-character(MAX_STR) :: s
-! If 's' is too short (MAX_STR too small), Fortan will abort with:
-! "Fortran runtime error: End of record"
-write(s, fmt) r
-sz = len_trim(s)
-end function
-
-pure function str_real(r) result(s)
-! Converts the real number "r" to string with 7 decimal digits.
-real(dp), intent(in) :: r
-character(len=*), parameter :: fmt="(f0.6)"
-character(len=str_real_len(r, fmt)) :: s
-write(s, fmt) r
-end function
-
-pure function str_real_n(r, n) result(s)
-! Converts the real number "r" to string with 'n' decimal digits.
-real(dp), intent(in) :: r
-integer, intent(in) :: n
-character(len=str_real_len(r, "(f0." // str_int(n) // ")")) :: s
-write(s, "(f0." // str_int(n) // ")") r
 end function
 
 end module
