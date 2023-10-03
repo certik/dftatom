ab026702c8536e3fb84dabde9eec58f80bead1a3
Thirumalai Shaktivel <thirumalaishaktivel@gmail.com>
2023-10-03 09:09:49 +0530

    Declare return variable as a pointer

--- src/mixings.f90
+++ src/mixings.f90
@@ -89,7 +89,7 @@ logical, intent(in) :: energy_crit
 type(dft_data_t), intent(inout) :: d ! Data passed to "F"
 real(dp), intent(in) :: alpha
 real(dp), intent(in) :: eps
-real(dp) :: mixing_anderson(size(x0))
+real(dp), pointer :: mixing_anderson(:)
 interface
     function R(x, i, d)
     use types
@@ -109,6 +109,7 @@ real(dp) :: ks_energies(size(d%ks_energies))
 real(dp) :: x_i_norm, R_i_norm
 real(dp) :: err_old, err, L2_err
 integer :: i
+allocate(mixing_anderson(size(x0)))
 x_i = x0
 if (energy_crit) then
     ks_energies = d%ks_energies
